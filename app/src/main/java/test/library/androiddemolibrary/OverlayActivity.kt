f3701f4501904b9a95226a8ea57b6ccd6c828b02
Roland Soos <roland@noteamyet.com>
2021-12-22 14:22:02 +0200

    commit

---
+++ app/src/main/java/test/library/androiddemolibrary/OverlayActivity.kt
@@ -0,0 +1,11 @@
+package test.library.androiddemolibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OverlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overlay)
+    }
+}
